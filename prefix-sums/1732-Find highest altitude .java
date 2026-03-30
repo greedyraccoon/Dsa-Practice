0903323e06e93831cea88145bc0549f949bd529f
greedyraccoon <119867452+greedyraccoon@users.noreply.github.com>
2026-03-30 23:46:51 +0530

    solved 1732 prefix problem

---
+++ prefix-sums/1732-Find highest altitude .java
@@ -0,0 +1,15 @@
+//1732-Find highest altitude
+//presum easy problem 
+
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int maxAlt = 0;
+        int currentAlt = 0;
+        for (int i = 0; i < gain.length; i++) {
+            currentAlt += gain[i];
+            if (currentAlt > maxAlt) maxAlt = currentAlt; 
+        }
+        
+        return maxAlt;
+    }
+}
