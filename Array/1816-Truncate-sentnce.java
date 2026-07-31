36adeb5a60264d506de6cc186bfedf9d6f7e0c91
greedyraccoon <119867452+greedyraccoon@users.noreply.github.com>
2026-07-31 23:19:24 +0530

    Add truncateSentence method in Solution class
    Implement truncateSentence method to return the first k words of a given string.

---
+++ Array/1816-Truncate-sentnce.java
@@ -0,0 +1,15 @@
+//1816-Truncate-sentnce
+//String array easy problem 
+
+class Solution {
+    public String truncateSentence(String s, int k) {
+        
+        String[] words = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < k; i++) {
+            sb.append(words[i]);
+            if(i < k - 1) sb.append(" ");
+        }
+        return sb.toString();
+    }
+}
